91a8a7118dc7437de9a7f69c25be6322dd4bc61a
filippopendyk <filippopendyk@gmail.com>
2025-07-28 12:17:44 -0400

    creates a carousel of 3 items in shelters category in admin dashboard, creates a chunkArray helper func

--- frontend/src/components/AdminSheltersCarousel/ManageSheltersCarousel.jsx
+++ frontend/src/components/AdminSheltersCarousel/ManageSheltersCarousel.jsx
@@ -3,18 +3,41 @@ import {
 } from "react-bootstrap";
 import { useContext } from "react";
 import SheltersContext from "../../../context/SheltersContext";
+import chunkArray from "../../helpers/chunkArray";
 
 const ManageSheltersCarousel = () => {
     const shelters = useContext(SheltersContext);
 
-    console.log(shelters);
+    const shelterChunks = chunkArray(shelters, 3); // 3 per page
+
     return (
         <div>
-            <h1 className="text-center fw-bold my-2"> 
-                Shelters
-            </h1>
+            <h1 className="text-center fw-bold my-2">Shelters</h1>
+            <div className="bg-dark bg-opacity-25 container-fluid">
+                <Carousel>
+                    {shelterChunks.map((chunk, index) => (
+                        <Carousel.Item key={index} style={{ height: 300 }}>
+                            <Stack
+                                direction="horizontal"
+                                className="h-100 justify-content-center align-items-center"
+                                gap={3}
+                            >
+                                {chunk.map((shelter, idx) => (
+                                    <Card key={idx} style={{ width: "18rem" }}>
+                                        <Card.Body>
+                                            <Card.Title>{shelter.name}</Card.Title>
+                                            <Card.Text>{shelter.description}</Card.Text>
+                                            <Button variant="primary">Edit</Button>
+                                        </Card.Body>
+                                    </Card>
+                                ))}
+                            </Stack>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ManageSheltersCarousel;
