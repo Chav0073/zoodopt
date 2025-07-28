8f1709a014c0b938af1b30898e3e1ab47b697dcd
filippopendyk <filippopendyk@gmail.com>
2025-07-28 17:55:00 -0400

    creates a medium shelters carousel with styling

--- frontend/src/components/ManageSheltersCarousel/ManageSheltersCarousel.jsx
+++ frontend/src/components/ManageSheltersCarousel/ManageSheltersCarousel.jsx
@@ -6,45 +6,52 @@ import SheltersContext from "../../../context/SheltersContext";
 import chunkArray from "../../helpers/chunkArray";
 import { useWindowSize } from '@uidotdev/usehooks';
 import SheltersCarouselSm from "../SheltersCarouselSm/SheltersCarouselSm";
+import SheltersCarouselMd from "../SheltersCarousuelMd/SheltersCarouselMd";
 
 const ManageSheltersCarousel = () => {
     const shelters = useContext(SheltersContext);
     const size = useWindowSize();
 
-    const shelterChunks = chunkArray(shelters, 3); // 3 per page
+    const shelter3Chunks = chunkArray(shelters, 3); 
+    const shelter2Chunks = chunkArray(shelters, 2);
 
     if(size.width <= 576){
         return <SheltersCarouselSm shelters={shelters}/>
     }
 
+    if(size.width > 576 && size.width <= 992){
+        return <SheltersCarouselMd shelters={shelter2Chunks} />
+    }
+
     return (
-        <div>
-            <h1 className="text-center fw-bold my-2">Shelters</h1>
-            <p>{size.width}</p>
-            <div className="container-fluid">
-                <Carousel>
-                    {shelterChunks.map((chunk, index) => (
-                        <Carousel.Item key={index} style={{ height: 300 }}>
-                            <Stack
-                                direction="horizontal"
-                                className="h-100 ms-5 align-items-center"
-                                gap={3}
-                            >
-                                {chunk.map((shelter, idx) => (
-                                    <Card key={idx} className="h-90" style={{ width: "18rem" }}>
-                                        <Card.Body>
-                                            <Card.Title>{shelter.name}</Card.Title>
-                                            <Card.Text>{shelter.description}</Card.Text>
-                                            <Button variant="primary">Edit</Button>
-                                        </Card.Body>
-                                    </Card>
-                                ))}
-                            </Stack>
-                        </Carousel.Item>
-                    ))}
-                </Carousel>
-            </div>
-        </div>
+        <p>Big screen</p>
+        // <div>
+        //     <h1 className="text-center fw-bold my-2">Shelters</h1>
+        //     <p>{size.width}</p>
+        //     <div className="container-fluid">
+        //         <Carousel>
+        //             {shelterChunks.map((chunk, index) => (
+        //                 <Carousel.Item key={index} style={{ height: 300 }}>
+        //                     <Stack
+        //                         direction="horizontal"
+        //                         className="h-100 ms-5 align-items-center"
+        //                         gap={3}
+        //                     >
+        //                         {chunk.map((shelter, idx) => (
+        //                             <Card key={idx} className="h-90" style={{ width: "18rem" }}>
+        //                                 <Card.Body>
+        //                                     <Card.Title>{shelter.name}</Card.Title>
+        //                                     <Card.Text>{shelter.description}</Card.Text>
+        //                                     <Button variant="primary">Edit</Button>
+        //                                 </Card.Body>
+        //                             </Card>
+        //                         ))}
+        //                     </Stack>
+        //                 </Carousel.Item>
+        //             ))}
+        //         </Carousel>
+        //     </div>
+        // </div>
     );
 };
 
