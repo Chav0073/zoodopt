51d09792084c0d1459b2682ff316834b3f458c69
filippopendyk <filippopendyk@gmail.com>
2025-07-28 17:55:00 -0400

    creates a medium shelters carousel with styling

--- frontend/src/components/SheltersCarousuelMd/SheltersCarouselMd.jsx
+++ frontend/src/components/SheltersCarousuelMd/SheltersCarouselMd.jsx
@@ -5,33 +5,42 @@ import { useContext } from "react";
 import SheltersContext from "../../../context/SheltersContext";
 import chunkArray from "../../helpers/chunkArray";
 import { useWindowSize } from '@uidotdev/usehooks';
-import SheltersCarouselSm from "../SheltersCarouselSm/SheltersCarouselSm";
 
-const ManageSheltersCarousel = () => {
-    const shelters = useContext(SheltersContext);
-    const shelterChunks = chunkArray(shelters, 2); // 3 per page
+const SheltersCarouselMd = ({shelters}) => {
 
     return (
         <div>
-            <h1 className="text-center fw-bold my-2">Shelters</h1>
-            <p>{size.width}</p>
             <div className="container-fluid">
                 <Carousel>
-                    {shelterChunks.map((chunk, index) => (
-                        <Carousel.Item key={index} style={{ height: 300 }}>
+                    {shelters.map((shelter, index) => (
+                        <Carousel.Item key={index} style={{ height: 500 }}>
                             <Stack
                                 direction="horizontal"
                                 className="h-100 justify-content-center align-items-center"
                                 gap={3}
                             >
-                                {chunk.map((shelter, idx) => (
-                                    <Card key={idx} style={{ width: "18rem" }}>
-                                        <Card.Body>
-                                            <Card.Title>{shelter.name}</Card.Title>
-                                            <Card.Text>{shelter.description}</Card.Text>
-                                            <Button variant="primary">Edit</Button>
-                                        </Card.Body>
-                                    </Card>
+                                {shelter.map((shelter, idx) => (
+                                    <Card className="shelter-card" key={shelter.id || idx}>
+                                    <Card.Img
+                                    variant="top"
+                                    src="../../../public/images/shelterimg.jpg"
+                                    alt="shelter image"
+                                    className="card-img-top"
+                                    />
+                                    <Card.Body>
+                                    <Card.Title>{shelter.name}</Card.Title>
+                                    <Card.Text className="card-text">
+                                        {shelter.phone}
+                                    </Card.Text>
+                                    <Card.Text className="card-text">
+                                        {shelter.description}
+                                    </Card.Text>
+                                    <Card.Text className="card-text">
+                                        {shelter.location}
+                                    </Card.Text>
+                                    <Button variant="primary">Edit</Button>
+                                    </Card.Body>
+                                </Card>
                                 ))}
                             </Stack>
                         </Carousel.Item>
@@ -42,4 +51,4 @@ const ManageSheltersCarousel = () => {
     );
 };
 
-export default ManageSheltersCarousel;
+export default SheltersCarouselMd;
