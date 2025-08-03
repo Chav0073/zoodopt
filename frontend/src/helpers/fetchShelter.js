f370cc28a4b25c8d4b8db08e31ffe288d2f0870b
filippopendyk <filippopendyk@gmail.com>
2025-08-02 22:08:15 -0400

    creates helper functions to fetch shelters, fetch shelter and getToken

---
+++ frontend/src/helpers/fetchShelter.js
@@ -0,0 +1,23 @@
+const fetchShelter = async (token, shelterId) => {
+    try {
+        const response = await fetch(`http://localhost:5217/shelters/${shelterId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+        });
+
+        if(!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log("Protected data: ", data);
+        return data;
+    } catch (error) {
+        console.log("Failed to fetch protected data: ", error.message);
+    }
+}
+
+export default fetchShelter;
