a7bf3458d72ace49c8ac865214bc5641b032ee1a
filippopendyk <filippopendyk@gmail.com>
2025-08-03 15:33:52 -0400

    updates the carousels for corresponding screens

--- frontend/src/components/PetsCarousel/PetsCarousel.jsx
+++ frontend/src/components/PetsCarousel/PetsCarousel.jsx
@@ -1,8 +1,7 @@
 import {
     Carousel, Card, Stack, Button
 } from "react-bootstrap";
-import { useContext } from "react";
-import PetsContext from "../../../context/PetsContext";
+import { useState, useEffect } from "react";
 import chunkArray from "../../helpers/chunkArray";
 import { useWindowSize } from '@uidotdev/usehooks';
 import PetsCarouselSm from "../PetsCarouselSm/PetsCarouselSm";
@@ -11,13 +10,27 @@ import PetsCarouselLg from "../PetsCarouselLg/PetsCarouselLg";
 import "./PetsCarousel.css";
 import { useNavigate } from 'react-router';
 import CreatePetBtn from "../CreatePetBtn/CreatePetBtn";
+import fetchPets from "../../helpers/fetchPets"; // Adjust the path accordingly
 
 const PetsCarousel = () => {
-    const pets = useContext(PetsContext);
+    const [pets, setPets] = useState([]);
     const size = useWindowSize();
     const navigate = useNavigate();
 
-    const pets3Chunks = chunkArray(pets, 3); 
+    useEffect(() => {
+        const getPets = async () => {
+            try {
+                const data = await fetchPets();
+                setPets(data);
+            } catch (error) {
+                console.error("Error fetching pets:", error);
+            }
+        };
+
+        getPets();
+    }, []);
+
+    const pets3Chunks = chunkArray(pets, 3);
     const pets2Chunks = chunkArray(pets, 2);
 
     const handleClick = (e) => {
@@ -31,13 +44,13 @@ const PetsCarousel = () => {
                 <h2 className="mb-0 fw-bold text-primary">Pets</h2>
                 <div>
                     <CreatePetBtn />
-                    <Button variant="outline-primary" className="fw-semibold px-4 py-2" onClick={(e) => handleClick(e)}>
+                    <Button variant="outline-primary" className="fw-semibold px-4 py-2" onClick={handleClick}>
                         Pets List
                     </Button>
                 </div>
             </div>
             {size.width <= 576 ? (
-                <PetsCarouselSm pets={pets}/>
+                <PetsCarouselSm pets={pets} />
             ) : size.width > 576 && size.width <= 992 ? (
                 <PetsCarouselMd pets={pets2Chunks} />
             ) : (
