29245219cefaee86b91bdb02199f4c6f6ae526d2
buen0023 <buen0023@algonquinlive.com>
2025-08-03 20:43:27 -0400

    End of all the branchs

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,74 @@
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../../../public/images/logo.svg";
+
 const NavBar = () => {
-    return <>
-        <p>Navbar</p>
-    </>
-}
+  return (
+    <Navbar
+      bg="light"
+      variant="light"
+      expand="lg"
+      sticky="top"
+      className="shadow-sm"
+    >
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>
+            <img
+              src={logo}
+              alt="Zoodopt Logo"
+              height="40"
+              className="d-inline-block align-top me-2"
+            />
+          </Navbar.Brand>
+        </LinkContainer>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+
+            {/* Pets Section */}
+            <NavDropdown title="Pets" id="pets-dropdown">
+              <LinkContainer to="/">
+                <NavDropdown.Item>Browse All Pets</NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to="/admin/pets">
+                <NavDropdown.Item>Manage Pets</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+
+            {/* Admin Section */}
+            <NavDropdown title="Admin" id="admin-dropdown">
+              <LinkContainer to="/admin">
+                <NavDropdown.Item>Dashboard</NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to="/admin/shelters">
+                <NavDropdown.Item>Manage Shelters</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/admin/users">
+                <NavDropdown.Item>Manage Users</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+          </Nav>
+
+          {/* Right side navigation */}
+          <Nav>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/register">
+              <Nav.Link>Register</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar;
