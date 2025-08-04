07aa1e3cf7aab894aed87f848b50eb346c5c8cc4
filippopendyk <filippopendyk@gmail.com>
2025-08-04 03:04:02 -0400

    creates a helpers for fetching users and user

---
+++ frontend/src/helpers/fetchUser.js
@@ -0,0 +1,22 @@
+const fetchUser = async (token, userId) => {
+    try {
+        const response = await fetch(`http://localhost:5217/users/${userId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+        });
+
+        if(!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log("Failed to fetch protected data: ", error.message);
+    }
+}
+
+export default fetchUser;
